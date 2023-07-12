7364f913a2cd1d60c34c23e48791d895f540387c
Evgen Shramkov <evgenshramkov@gmail.com>
2023-07-12 20:40:52 +0300

    Global chenge

---
+++ HellowUkraine.java
@@ -0,0 +1,74 @@
+public class HellowUkraine {
+    public static void main(String[] args) {
+        System.out.println("Hello Ukraine2. \nJava \n Is \n Great\" ");
+        System.out.print("Prints a character. \n The character is translated into one or more bytes" +
+                " according to the character encoding given to the constructor, " +
+                "or the default charset if none specified. " +
+                "\n These bytes are written in exactly the manner of the write(int) method.");
+        System.out.print("222222 The character is");
+        System.out.print("   translated into one or more bytes according");
+        System.out.print(" = ");
+        //Ось кілька прикладів використання класів-обгорток:
+        Integer b; //дозволяє "зчитати" число із рядка
+        b = Integer.parseInt("120");
+        Boolean k = Boolean.FALSE;
+        Long a = 150L;
+        System.out.println(a + b); // отримаємо 13
+        System.out.print("Оператор присвоєння = ");
+        //Оператор присвоєння
+        double appleCount = 50.5; //Просто присвоїли число 50
+        appleCount += 50.5; //додаєм до значення змінної 50.5
+        int bananaCount = 8 + 12; //Присвоїли 11 - суму 8 і 12
+        double totalFruitCount = appleCount / bananaCount; // Присвоїли суму змінних appleCount і bananaCount - 70
+        System.out.println(totalFruitCount);
+        System.out.println("Оператор -= віднімає від операнда значення праворуч:");
+        int weekDays = 7;
+        weekDays -= appleCount;
+        System.out.print("Кількість робочих днів в тижні = ");
+        System.out.println(weekDays);
+        int notDictributedItems = bananaCount + weekDays % 3;
+        System.out.print(notDictributedItems);
+        System.out.println("  $ Значення змінних");
+        System.out.println(appleCount);
+        System.out.println("appleCount");
+        System.out.println(bananaCount);
+        System.out.println(weekDays);
+        System.out.println(notDictributedItems);
+        //Оператори порівняння
+        //* оператори рівності
+        boolean summFruitCount = appleCount == bananaCount; //Буде false, тому що обидва операнди рівні
+        System.out.println(summFruitCount);
+        char bigA = 'A';
+        char smallA = 'f';
+        boolean theSameLatter = bigA == smallA; //Буде true, тому що обидва операнди рівні
+        System.out.println(theSameLatter);
+        //* оператори не рівності
+        System.out.println("оператори не рівності");
+        boolean isDifferent = appleCount != bananaCount; //Буде true, тому що обидва операнди нерівні
+        // оператори більше меньше
+        System.out.println("Оператори більше меньше");
+        System.out.println(appleCount > bananaCount);
+        System.out.println(appleCount < bananaCount);
+        System.out.println(appleCount >= bananaCount);
+        //Логічні оператори
+        System.out.println("Логічні оператори");
+        boolean x = true;
+        boolean y = false;
+        boolean z = false;
+        boolean xOrY = x || y;
+        boolean yOrZ = y || z || xOrY;
+        boolean xOrYz = x || yOrZ;
+        System.out.println("1. Результат");
+        System.out.println(xOrY);
+        System.out.println(yOrZ);
+        System.out.println(xOrYz);
+        System.out.println("Завершення прогарми");
+        // boolean texUkr =
+        System.out.print("Початок нового розділу");
+    }
+
+// public int return () {
+//return sum = sum(a, b);
+//  AtomicInteger sum = new AtomicInteger();
+//return 0;
+}
