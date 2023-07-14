07f80620cb472aeb3cd57e1eff71f5340face350
Evgen Shramkov <evgenshramkov@gmail.com>
2023-07-14 16:55:14 +0300

    11

--- Maine.java
+++ Maine.java
@@ -1,17 +1,21 @@
 public class Maine {
         public static void main(String[] args) {
         int a;
-        a = 5;
+        a = 2;
         int b;
-        b = 7;
+        b = 8;
         int result = a + b;
        System.out.println("Result: " + result);
-    }
+            Calculator cal = new Calculator( 10, 15);
+        }
 /*
     public static int add( {
         return add(9, 10);
     }
     //int result = add(a: 1, b: 2);
+    System.out.println("Result: " + result);
+            Calculator cal = new Calculator( 10, 15);
+        }
     public static int add(
             ) {
         return a + b;
