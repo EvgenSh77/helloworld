2a0becbc4c38b20d7931d04dd9a71b617cb884bb
Evgen Shramkov <evgenshramkov@gmail.com>
2023-07-14 16:54:58 +0300

    11

---
+++ HellowPeople.java
@@ -0,0 +1,10 @@
+public class HellowPeople {
+    public static void main(String [] args) {
+        System.out.println("Hellow many people");
+    
+            // boolean texUkr =
+            System.out.print("Початок нового розд!лу");
+        // boolean texUkr =
+        System.out.print("\nПочаток нового пiдроздiлу");
+}
+  }
