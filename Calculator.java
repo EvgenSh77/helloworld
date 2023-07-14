0ff0e9ae41c2c25a7ed0bf9a7daa7cfc25e6eaea
Evgen Shramkov <evgenshramkov@gmail.com>
2023-07-14 16:52:34 +0300

    hellowPeople

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+    public  int haight;
+    public int widht;
+    Calculator(int haight, int widht){
+        this.haight = haight;
+        this.widht = widht;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Maine.java
@@ -0,0 +1,22 @@
+public class Maine {
+        public static void main(String[] args) {
+        int a;
+        a = 5;
+        int b;
+        b = 7;
+        int result = a + b;
+       System.out.println("Result: " + result);
+    }
+/*
+    public static int add( {
+        return add(9, 10);
+    }
+    //int result = add(a: 1, b: 2);
+    public static int add(
+            ) {
+        return a + b;
+        public static int add(     int a,
+        int b
+
+ */
+    }
